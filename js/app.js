50ac4f58979f8e4939955e7c5f2b0ea34658c1fb
kbudd1234 <kevinpmbudd@gmail.com>
2015-10-07 16:08:56 -0500

    Feat: enemies are properly plaved and moving. Player can move over the board. Want to incorporate dt into players movement. Collision detection needs implementing as well as winning the game and restarting.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Enemy = function(pos) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
+    this.x = pos[0];
+    this.y = pos[1];
+    this.speed = 100 + Math.floor(Math.random() * 150);
 };
 
 // Update the enemy's position, required method for game
@@ -17,10 +17,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if (this.x > 99 && this.x < 506)
-        this.x += 150 * dt;
-    else
-        this.x = 50;
+    this.x = this.x + this.speed * dt;
+
+    if (this.x > 505) {
+        this.x = 0;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -31,19 +32,20 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y) {
+var Player = function(pos) {
     this.sprite = 'images/char-boy.png';
-    this.x = x;
-    this.y = y;
+    this.x = pos[0];
+    this.y = pos[1];
 }
 
+var delta;
 // Update the player's position, required method for game
 // Parameter: dt, a time delta between ticks
 Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-
+    delta = dt;
 };
 
 // Draw the player on the screen, required method for game
@@ -55,16 +57,24 @@ Player.prototype.render = function() {
 Player.prototype.handleInput = function (move) {
     switch (move) {
         case 'left':
-            player.x = player.x - 100;
+            if (this.x > 0) {
+                this.x = this.x - 100;
+            }
             break;
         case 'up':
-            player.y = player.y - 85;
+            if (this.y > 0) {
+                this.y = this.y - 85;
+            }
             break;
         case 'right':
-            player.x = player.x + 100;
+            if (this.x < 400) {
+                this.x = this.x + 100;
+            }
             break;
         case 'down':
-            player.y = player.y + 85;
+            if (this.y < 350) {
+                this.y = this.y + 85;
+            }
             break;
     }
 
@@ -74,11 +84,12 @@ Player.prototype.handleInput = function (move) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-for (var nme = 1; nme < 5; nme++) {
-    allEnemies.push(new Enemy(100 ,100*nme ,500));
-};
+for(var i=1; i<4; i++){
+    var enemy = new Enemy([0-i*101, 83*i-21]);
+    allEnemies.push(enemy);
+}
 
-var player = new Player(200,300);
+var player = new Player([200,300]);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
